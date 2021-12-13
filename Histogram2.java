3fcaeb7e58e48a4bb7ca968db45c3d1af93b2c60
Abel <91392893+Magnasin@users.noreply.github.com>
2021-12-13 15:31:37 -0500

    Add files via upload

---
+++ Histogram2.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package histogram;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author abeld
+ */
+public class Histogram2 {
+    
+/**
+* @param args the command line arguments
+*/
+  
+public static void main(String[] args) throws IOException {
+
+
+File file2 = new File("test2.txt");//File location is past as the path to the file as a parameter
+Scanner scanFile = new Scanner(file2); //Creates a scanner for the file
+
+ArrayList<Integer> integerValues = new ArrayList<Integer>(); //Creates an array list to store integers
+  
+int otherChar = 0;
+  
+int counting = 0;
+  
+try{
+    
+while( scanFile.hasNext() ){ //Reads input file
+         
+String reads = scanFile.nextLine();      
+boolean highlight = true;
+      
+for(int i=0; i<reads.length(); i++) {//Notes if the inputed value is Integer or not
+          
+if(!Character.isDigit(reads.charAt(i))) {
+              
+otherChar ++;             
+highlight = false;              
+break;
+              
+}
+       
+}
+      
+if(highlight) {//Checks if it is then add to list
+          
+int num = Integer.parseInt(reads);          
+integerValues.add(num);
+                  
+}     
+}
+}
+   
+finally {
+scanFile.close();//Closes scanner in the file to prevent data loss
+}
+  
+int variable[] = new int[10]; // Array with 11 spaces
+  
+for(int i=0; i<integerValues.size(); i++) { //For loop to check the input file 
+      
+if(integerValues.get(i) >=1 && integerValues.get(i) <=10 ) {//Checks the input and adds it to the array   
+          
+variable[0]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=11 && integerValues.get(i) <=20 ) {  //Checks range of all integers
+          
+variable[1]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=21 && integerValues.get(i) <=30 ) {  //Checks range of all integers
+          
+variable[2]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=31 && integerValues.get(i) <=40 ) {  //Checks range of all integers
+          
+variable[3]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=41 && integerValues.get(i) <=50 ) {  //Checks range of all integers
+          
+variable[4]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=51 && integerValues.get(i) <=60 ) {  //Checks range of all integers
+          
+variable[5]++;
+counting++;
+
+}
+      
+else if(integerValues.get(i) >=61 && integerValues.get(i) <=70 ) {  //Checks range of all integers
+          
+variable[6]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=71 && integerValues.get(i) <=80 ) {  //Checks range of all integers
+          
+variable[7]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=81 && integerValues.get(i) <=90 ) {  //Checks range of all integers
+          
+variable[8]++;
+counting++;
+          
+}
+      
+else if(integerValues.get(i) >=91 && integerValues.get(i) <=100 ) {  //Checks range of all integers
+          
+variable[9]++;
+counting++;
+          
+}
+      
+else {
+  
+otherChar++;  //Checks range of all other characters
+  
+}      
+}
+        
+FileWriter writer2 = new FileWriter("result1-2.txt"); //Uses the FileWriter tool
+    
+writer2.write("Reading Integers from the file: C:\\Users\\abeld\\Desktop\\School Work\\COP 3337.1\\Assignment3\\test2" + "\n");//Prints file location in the output file
+  
+writer2.write("Numbers of Integers in Interval [1-100]: " + counting + "\n"); //Prints the interval
+  
+writer2.write("Other Characters: " + otherChar + "\n"); // Prints the other characters
+  
+writer2.write("Histogram"); // Prints Histogram
+  
+int minimum = 1;
+int maximum = 10;
+    
+for(int i=0;i<variable.length;i++) {//For loop to print the histogram to the output file
+      
+writer2.write("\n" + minimum + "-" + maximum + " | ");
+      
+for(int j=0; j<variable[i];j++) {
+          
+writer2.write("*");
+          
+}
+     
+minimum = minimum+10;
+maximum = maximum+10;
+         
+}
+  
+writer2.close();//Closes writer2 to prevent data loss
+ 
+}
+} 
